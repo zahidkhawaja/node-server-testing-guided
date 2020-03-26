539fd8f1b19d2a332f9f0cfa2cfb3d7db092d3e2
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-26 10:46:47 +0100

    wip

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,3 +1,18 @@
+const server = require('./server')
+const request = require('supertest')
+
 it('is running with the correct db', () => {
   expect(process.env.DB_ENV).toBe('testing')
+})
+
+describe('server.js', () => {
+  describe('[GET] /', () => {
+    it('runs correctly', () => {
+      return request(server).get('/')
+    })
+  })
+
+  describe('[GET] /hobbits', () => {
+
+  })
 })
